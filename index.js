bbcfcc82fb77d338a0d8d718a278f9f58802f8c3
jacobpeter5663-hub <jacobpeter5663@gmail.com>
2026-08-27 15:41:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+1.
+let studentName = "peter";
+let age = 27;
+let mark = 95;
+
+console.log(studentName);
+console.log(age);
+console.log(mark);
+2.
+let username = prompt("Enter your name");
+alert(username);
+
+let userage = prompt("Enter your age");
+console.log("userage");
+4.
+let question = confirm("Are you eligible to vote");
+console.log(question);
+
+5.
+ username = prompt("Enter your name");
+document.writein("username");
