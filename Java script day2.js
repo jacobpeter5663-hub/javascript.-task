181900ef6d5850979c3c683de1699fa9801cf976
jacobpeter5663-hub <jacobpeter5663@gmail.com>
2026-08-31 19:50:41 +0530

    Implement JavaScript tasks for learning purposes
    Added multiple tasks demonstrating JavaScript concepts including variables, data types, functions, and basic operations.

---
+++ Java script day2.js
@@ -0,0 +1,288 @@
+Task 1
+var studentName = "Naveen";
+let studentAge = 22;
+const collegeName = "ABC College";
+
+console.log(studentName);
+console.log(studentAge);
+console.log(collegeName);
+studentName = "Kumar";
+console.log(studentName);
+studentAge = 23;
+console.log(studentAge);
+collegeName = "XYZ College";
+console.log(studentName);
+let studentAge = 25;
+
+Task 2
+let name = prompt("Enter your name:");
+let age = prompt("Enter your age:");
+let city = prompt("Enter your city:");
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("City:", city);
+
+Task 3
+let name = prompt("Enter your name:");
+alert("Welcome " + name + "!");
+
+Task 4
+let birthYear = prompt("Enter your birth year:");
+let age = 2029 - birthYear;
+console.log("Birth Year:", birthYear);
+console.log("Age:", age);
+
+Task 5
+let a = "Hiii";
+let b = 1000;
+let c = 25.5;
+let d = true;
+let e = false;
+let f = undefined;
+let g = null;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
+
+Task 6
+let student = {
+    name: "Vimal",
+    age: 21,
+    city: "Salem",
+    qualification: "BCOM",
+    isStudent: true};
+console.log(student);
+console.log(student.name);
+console.log(student.age);
+console.log(student.qualification);
+console.log(student.isStudent);
+
+Task 7
+let fruits = ["Apple", "Mango", "Orange", "Banana", "Grapes", "Papaya"];
+console.log("First fruit:", fruits[0]);
+console.log("Second fruit:", fruits[1]);
+console.log("Last fruit:", fruits[fruits.length - 1]);
+console.log("Total fruits:", fruits.length);
+
+Task 8
+let a = 20;
+let b = 5;
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
+console.log("Exponentiation:", a ** b);
+
+Task 9
+let shirt = 999;
+let pant = 1499;
+let shoes = 1999;
+let total = shirt + pant + shoes;
+console.log("Total =", total);
+
+Task 10
+let tamil = 80;
+let english = 75;
+let maths = 90;
+let total = tamil + english + maths;
+let average = total / 3;
+console.log("Total Marks:", total);
+console.log("Average Marks:", average);
+
+Task 11
+let a = 10;
+let b = a++;
+console.log(a);
+console.log(b);
+
+Task 12
+let a = 10;
+let b = ++a;
+console.log(a);
+console.log(b);
+
+Task 13
+let a = 20;
+let b = a--;
+console.log(a);
+console.log(b);
+
+Task 14
+let a = 20;
+let b = --a;
+console.log(a);
+console.log(b);
+
+Task 15
+let a = 5;
+let b = a++;
+let c = ++a;
+let d = b--;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+Task 16
+let num = 10;
+num += 5;
+console.log(num);
+
+let num = 10;
+num -= 5;
+console.log(num);
+
+let num = 10;
+num *= 5;
+console.log(num);
+
+let num = 10;
+num /= 5;
+console.log(num);
+
+let num = 10;
+num %= 5;
+console.log(num);
+
+let num = 10;
+num **= 2;
+console.log(num);
+
+Task 17
+
+let name = "Nirvan";
+let age = 21;
+let city = "Salem";
+let college = "XYZ College";
+
+let subjects = ["Tamil", "English", "Maths", "Science", "Computer"];
+
+let student = {
+    name: name,
+    age: age,
+    city: city,
+    subjects: subjects,
+    isStudent: true };
+console.log("Student Name:", name);
+console.log("Student Age:", age);
+console.log("City:", city);
+console.log("First Subject:", subjects[0]);
+console.log("Last Subject:", subjects[subjects.length - 1]);
+console.log("Total Subjects:", subjects.length);
+console.log("Complete Object:", student);
+
+Final Challenge
+
+let a = Number(prompt("Enter first number:"));
+let b = Number(prompt("Enter second number:"));
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
+console.log("Power:", a ** b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
